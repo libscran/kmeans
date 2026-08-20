adef44521f1cdb2d6feb06aadfbecb84cde4ada4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-08-20 12:16:01 +1000

    Parallelize() now returns the number of used workers.
    This aligns with the behavior of the latest subpar::parallelize_range();

--- include/kmeans/parallelize.hpp
+++ include/kmeans/parallelize.hpp
@@ -20,17 +20,19 @@ namespace kmeans {
  * @param num_tasks Number of tasks.
  * @param run_task_range Function to iterate over a range of tasks within a worker.
  *
+ * @return Number of used workers \f$U\f$, where `run_task_range()` is called once per integer in \f$[0, U)\f$.
+ *
  * By default, this is an alias to `subpar::parallelize_range()`.
  * However, if the `KMEANS_CUSTOM_PARALLEL` function-like macro is defined, it is called instead. 
- * Any user-defined macro should accept the same arguments as `subpar::parallelize_range()`.
+ * Any user-defined macro should accept the same arguments as `subpar::parallelize_range()` and return the number of used workers.
  */
 template<typename Task_, class Run_>
-void parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
+int parallelize(const int num_workers, const Task_ num_tasks, Run_ run_task_range) {
 #ifndef KMEANS_CUSTOM_PARALLEL
     // Do NOT make this no-throw as we don't know whether Matrix_::get_observation() might throw.
-    subpar::parallelize(num_workers, num_tasks, std::move(run_task_range));
+    return subpar::parallelize_range(num_workers, num_tasks, std::move(run_task_range));
 #else
-    KMEANS_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
+    return KMEANS_CUSTOM_PARALLEL(num_workers, num_tasks, run_task_range);
 #endif
 }
 
